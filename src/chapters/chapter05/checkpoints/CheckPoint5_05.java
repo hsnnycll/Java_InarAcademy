cd8b10c1fbd2d818c6251391dd5a27194f4d42d0
Hasan Yücel <hsnnycll@gmail.com>
2022-09-07 14:05:28 +0300

    add new files

---
+++ src/chapters/chapter05/checkpoints/CheckPoint5_05.java
@@ -0,0 +1,14 @@
+package chapters.chapter05.checkpoints;
+
+public class CheckPoint5_05 {
+	public static void main(String[] args) {
+		
+		int x = 80000000;
+		
+		while (x > 0)
+			x++;
+			
+			System.out.println("x is " + x);
+	}
+
+}
