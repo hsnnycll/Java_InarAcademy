00ef5a35927431d58178d52667a15c275950409f
Hasan Yücel <hsnnycll@gmail.com>
2022-09-29 13:20:39 +0300

    add new exercise

--- src/chapters/chapter06/exercises/C06E10.java
+++ src/chapters/chapter06/exercises/C06E10.java
@@ -1,5 +1,24 @@
 package chapters.chapter06.exercises;
 
+import static chapters.chapter06.listings.PrimeNumberMethod.isPrime;
+
 public class C06E10 {
-    
+    public static void main(String[] args) {
+
+        System.out.println("The prime numbers less than 10000 are; ");
+        final int NUMBER_OF_PRIMES_PER_LINE = 10;
+        int count = 0;
+        int number = 2;
+        int lastNumber = 10000;
+        while (number < lastNumber) {
+            if (isPrime(number)) {
+                count++;
+                if (count % NUMBER_OF_PRIMES_PER_LINE == 0) {
+                    System.out.printf("%-5s\n", number);
+                } else
+                    System.out.printf("%-5s", number);
+            }
+            number++;
+        }
+    }
 }
