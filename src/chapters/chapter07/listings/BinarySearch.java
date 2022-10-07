6a2460f61350f663d905252e95e101aef5f48c27
Hasan Yücel <hsnnycll@gmail.com>
2022-10-07 14:57:42 +0300

    add new listings

---
+++ src/chapters/chapter07/listings/BinarySearch.java
@@ -0,0 +1,33 @@
+package chapters.chapter07.listings;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+        int i = binarySearch(list, 2);
+        int j = binarySearch(list, 11);
+        int k = binarySearch(list, 12);
+        int l = binarySearch(list, 1);
+        int m = binarySearch(list, 3);
+
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(l);
+        System.out.println(m);
+    }
+    public static int binarySearch(int[] list, int key) {
+        int low = 0;
+        int high = list.length - 1;
+
+        while (high >= low) {
+            int mid = (low + high) / 2;
+            if (key < list[mid])
+                high = mid - 1;
+            else if (key == list[mid])
+                return mid;
+            else
+                low = mid + 1;
+        }
+        return - 1;
+    }
+}
