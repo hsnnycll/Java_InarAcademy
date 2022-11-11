e9afa9f39e93dbb59bc3e3d76f0a9314a7eca988
Hasan YUCEL <hsnnycll@gmail.com>
2022-11-11 09:33:03 +0300

    add exercise

--- src/chapters/chapter10/exercises/C10E17/SquareNumbers.java
+++ src/chapters/chapter10/exercises/C10E17/SquareNumbers.java
@@ -1,9 +1,24 @@
 package chapters.chapter10.exercises.C10E17;
 
+import java.math.BigDecimal;
+
 public class SquareNumbers {
     public static void main(String[] args) {
 
-        String str = String.valueOf(Long.MAX_VALUE);
-        System.out.println(str);
+        BigDecimal longMaxValue = new BigDecimal(Long.MAX_VALUE);
+        System.out.println("The max long value: " + longMaxValue);
+        BigDecimal number = new BigDecimal((long)Math.sqrt(Long.MAX_VALUE));
+
+        int count = 0;
+        while (count < 10) {
+            BigDecimal squared = number.multiply(number);
+            if (squared.compareTo(longMaxValue) > 0) {
+                System.out.println(count + ": " + number + " squared = " + squared);
+                count++;
+            }
+            number = number.add(BigDecimal.ONE);
+        }
     }
+
+
 }
---
+++ src/chapters/chapter10/exercises/C10E18/LargePrimeNumbers.java
@@ -0,0 +1,8 @@
+package chapters.chapter10.exercises.C10E18;
+
+public class LargePrimeNumbers {
+    public static void main(String[] args) {
+
+    }
+
+}
