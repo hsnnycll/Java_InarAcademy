16ae3ab036c301608067e56ebe73a8e975c92017
Hasan YUCEL <hsnnycll@gmail.com>
2022-11-13 11:10:37 +0300

    add listing

---
+++ src/chapters/chapter11/listings/DynamicBindingDemo.java
@@ -0,0 +1,33 @@
+package chapters.chapter11.listings;
+
+public class DynamicBindingDemo {
+    public static void main(String[] args) {
+
+        m(new GraduateStudent());
+        m(new Student());
+        m(new Person1());
+        m(new Object());
+    }
+
+    public static void m(Object x) {
+        System.out.println(x.toString());
+    }
+}
+
+class GraduateStudent extends Student {
+
+}
+
+class Student extends Person1 {
+    @Override
+    public String toString() {
+        return "Student";
+    }
+}
+
+class Person1 extends Object {
+    @Override
+    public String toString() {
+        return "Person";
+    }
+}
