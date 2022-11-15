cc7eea9a2ac847d6c748fa95fd967a92dacec1b6
Hasan YUCEL <hsnnycll@gmail.com>
2022-11-15 21:32:30 +0300

    restored exercise

--- src/chapters/chapter07/exercises/C07E06.java
+++ src/chapters/chapter07/exercises/C07E06.java
@@ -1,29 +1,35 @@
 package chapters.chapter07.exercises;
 
 public class C07E06 {
-    public static void main(String[] args) {
-        final int NUMBER_OF_PRIMES = 50;
-        final int NUMBER_OF_PRIMES_PER_LINE = 10;
-        int[] primeNumbers = new int[NUMBER_OF_PRIMES];
-
-        int index = 0;
-        for (int i = 2; index < NUMBER_OF_PRIMES; i++) {
-            if (isPrime(i))
-                primeNumbers[index++] = i;
+    public static boolean isPrime(int[] array, int count, int number) {
+        for (int i = 0; i < count; i++) {
+            if(array[i] <= Math.sqrt(number)) {
+                if (number % array[i] == 0) {
+                    return false;
+                }
+            }
         }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] primeNumbers = new int[50];
+        int number = 2;
+        int count = 0;
+
+        while(count < primeNumbers.length){
+            if(isPrime(primeNumbers,count, number)){
+                primeNumbers[count] = number;
+                count++;
+            }
+            number++;
+        }
+
         System.out.println("The first 50 prime number are: ");
         for (int i = 0; i < primeNumbers.length; i++) {
             System.out.printf("%4d", primeNumbers[i]);
-            if ((i + 1) % NUMBER_OF_PRIMES_PER_LINE == 0) System.out.println("");
+            if ((i + 1) % 10 == 0) System.out.println("");
         }
-    }
-
-    public static boolean isPrime(int i) {
-        for (int j = 2; j <= Math.sqrt(i); j++) {
-            if (i % j == 0)
-                return false;
-        }
-        return true;
     }
 }
 
