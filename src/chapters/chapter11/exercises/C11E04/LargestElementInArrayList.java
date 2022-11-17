8297a7dd00542329d2ebf822621fd0b43edda711
Hasan YUCEL <hsnnycll@gmail.com>
2022-11-17 11:12:46 +0300

    add exercise

---
+++ src/chapters/chapter11/exercises/C11E04/LargestElementInArrayList.java
@@ -0,0 +1,32 @@
+package chapters.chapter11.exercises.C11E04;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LargestElementInArrayList {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a sequence of numbers (ending wtih 0): ");
+        ArrayList<Integer> list = new ArrayList<>();
+        int number;
+        while ((number = input.nextInt()) != 0){
+            list.add(number);
+        }
+
+        System.out.println("The largest number is " + max(list));
+    }
+
+    public static Integer max(ArrayList<Integer> list){
+        if(list.isEmpty() || list == null){
+            return null;
+        }
+        int max = list.get(0);
+        for (int i = 1; i < list.size() - 1; i++) {
+            if(list.get(i) > max){
+                max = list.get(i);
+            }
+        }
+        return max;
+    }
+}
