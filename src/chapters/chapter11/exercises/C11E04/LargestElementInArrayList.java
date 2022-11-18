0b97e1a3079dc5436b7f9af5898ca65dd9acee1b
Hasan YUCEL <hsnnycll@gmail.com>
2022-11-18 11:00:28 +0300

    restored exercise

--- src/chapters/chapter11/exercises/C11E04/LargestElementInArrayList.java
+++ src/chapters/chapter11/exercises/C11E04/LargestElementInArrayList.java
@@ -22,7 +22,7 @@ public class LargestElementInArrayList {
             return null;
         }
         int max = list.get(0);
-        for (int i = 1; i < list.size() - 1; i++) {
+        for (int i = 1; i < list.size(); i++) {
             if(list.get(i) > max){
                 max = list.get(i);
             }
